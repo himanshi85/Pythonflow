37ceb4d9cb613903c8f8a6a6389eac86f1740a5b
himanshi85 <phimanshi009@gmail.com>
2026-01-21 11:44:52 +0530

    Added one line

---
+++ scientific_calculator/log_function.py
@@ -0,0 +1,6 @@
+print("Hello this is log function file")
+
+print("This file contains logarithmic functions for the scientific calculator.")
+
+
+print("This file contains logarithmic functions for the scientific calculator.")
